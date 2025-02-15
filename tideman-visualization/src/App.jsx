57c2864fb8fa505ae3f1c97d829976e16d202955
LuiDevv <tavarezluis2055@gmail.com>
2025-02-15 08:42:07 -0400

    UI applied, minor changes on app code

--- tideman-visualization/src/App.jsx
+++ tideman-visualization/src/App.jsx
@@ -1,30 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 // Pages
 import { Home } from './pages/Home';
-import {CreateElections} from './pages/CreateElections';
+import { CreateElections } from './pages/CreateElections';
 
 function App() {
-  return (
-    <>
-      <h2>AlGORITMO DE TIDEMAN PARA ELECCIONES CON VOTOS PLURISTAS</h2>
-      <p>Por grupo #5</p>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Home />}/>
-          <Route path="/create" element={ <CreateElections />} />
-        </Routes>
-      </BrowserRouter>
-      
-      
-    </>
-  )
+    return (
+        <div className="app-container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/create" element={<CreateElections />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
